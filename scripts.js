ad04270800494f9d88ffa2bc1c3db44865316094
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2021-03-14 05:18:30 -0500

    completed the pixelate button

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function pictureChange()
+{
+   document.getElementById("pixel-img").src="images/pixel-earth.png";
+   $('body').css("font-family", "'Press Start 2P'");
+   $('h1').css("font-family", "'Press Start 2P'");
+   $('h2').css("font-family", "'Press Start 2P'");
+   $('h3').css("font-family", "'Press Start 2P'");
+   $('h4').css("font-family", "'Press Start 2P'");
+   $('h5').css("font-family", "'Press Start 2P'");
+   $('p').css("font-family", "'Press Start 2P'");
+   $('a').css("font-family", "'Press Start 2P'");
+   
+    
+}
